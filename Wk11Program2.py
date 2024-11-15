8410f105d690053ab071c89ee2aa5d705457078c
M-Court <mcourt@students.unwsp.edu>
2024-11-15 17:05:54 -0600

    Update Wk11Program2.py

--- Wk11Program2.py
+++ Wk11Program2.py
@@ -1,1 +1,43 @@
 #program 2
+
+#2:  Write a GUI program that
+		#displays your name and address when a "Show Info" button is clicked.  
+		#There should also be a "Quit" button which exits the GUI.
+
+import tkinter
+
+class MyGUI:
+		__init__(self)
+				#create main window widget
+				self.main_window = tkinter.Tk()
+				
+				#			tkinter.button
+				
+				# show info (TEZTBOOk)
+				tkinter.messagebox.showinfo("My name is Mackenzie Court", "I live in Hendricks, MN.")
+				
+			
+			
+				#create a button widget. the text says "quit"
+				#the quit method should be executed when the user clicks the Button.
+				self.my_button = tkinter.Button(self.main_window, text='Click Me!')
+				command=self.do_something)
+			
+			
+				while True:
+					button. quit?	
+				
+
+#create main function
+def main():		
+		#create a main window widget
+		main_window = tkinter.Tk()
+				
+		#enter the tkinter main loop
+		tkinter.mainloop()
+								
+	
+#call the main function
+if __name__ == '__main__':
+		main()
+					
